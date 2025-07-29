a6b7c01e631291720849e8bfbe90a94cf45057f8
Ulugbek Abdulazizov <aqllibola567@gmail.com>
2025-07-29 10:44:41 +0500

    changed database from sqlite3 to postgresql

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uwvf#+ppb4t+9_&o5+mp*tr%u44$bvwy6j_&m9jv#o24evz#or'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-uwvf#+ppb4t+9_&o5+mp*tr%u44$bvwy6j_&m9jv#o24evz#or')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DJANGO_DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '*').split(',')
 
 
 # Application definition
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'database_name'),
+        'USER': os.getenv('DB_USER', 'database_user'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'database_password'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
